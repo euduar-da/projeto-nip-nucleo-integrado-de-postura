5c8c3beff43b0dd4fc74b7f175beccd295e8f905
euduar-da <mdasilvasouzs@gmail.com>
2026-06-13 15:57:54 -0300

    feat: adiciona token para buscar informações dos pacientes

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import LoginView, LogoutView, PacienteCadastroView, ColaboradorCadastroView
+from .views import LoginView, LogoutView, PacienteCadastroView, ColaboradorCadastroView, PacientePerfilView
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('pacientes/cadastrar/', PacienteCadastroView.as_view(), name='cadastrar-paciente'),
     path('colaboradores/cadastrar/', ColaboradorCadastroView.as_view(), name='cadastrar-colaborador'),
+    path('paciente/perfil/', PacientePerfilView.as_view(), name='paciente-perfil'),
 ]
